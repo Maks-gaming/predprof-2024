6bf501c410ed41588f74e781576812a7d22caae5
Maksim Klimenko <maksim.klimenko.mail@gmail.com>
2024-01-12 09:02:21 +0300

    Added "index => auth" redirect

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,11 +4,7 @@ const router = express.Router();
 
 // Главная страница
 router.get("/", async (_req, res) => {
-	return res.render("index.html", {
-		// Сюда содержимое (если не надо, убрать)
-		// Написав "test: 10" здесь, на сайте это
-		// значение можно получить через {{test}}
-	});
+	return res.redirect("/auth");
 });
 
 export default router;
